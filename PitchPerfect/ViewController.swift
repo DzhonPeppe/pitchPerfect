05e136e2b5572d389a26af9a44e02ec3a32a828c
@johnpeppe <johnpeppe@gmail.com>
2021-05-16 11:25:42 +0300

    Recording UI Assets Added

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,10 +10,13 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        stopRecordingButton.isEnabled = false
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -24,10 +27,16 @@ class ViewController: UIViewController {
     @IBAction func recordAudio(_ sender: Any) {
         print("record button was pressed")
         recordingLabel.text = "Recording in Progress"
+        stopRecordingButton.isEnabled = true
+        recordButton.isEnabled = false
     }
     
     @IBAction func stopRecording(_ sender: Any) {
         print("stop recording button was pressed")
+        recordingLabel.text = "Tap to Record"
+        recordButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
     }
+    
 }
 
